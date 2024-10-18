4174950ffce7fa7e6838784148ae71d114a37b8c
raviw45 <ravikantwaghmare82@gmail.com>
2024-10-18 21:53:59 +0530

    feat: Eslint and prettier setup

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
+  languageOptions: {
+    parserOptions: {
+      project: true,
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  files: ["**/*.ts"],
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+  ],
+  rules: {
+    "no-console": "error",
+    "no-useless-catch": 0,
+    quotes: ["error", "single", { allowTemplateLiterals: true }],
+    "@typescript-eslint/no-misused-promises": 0,
+    "@typescript-eslint/no-unsafe-call": 0,
+    "@typescript-eslint/no-unsafe-assignment": 0,
+    "@typescript-eslint/no-unsafe-member-access": 0,
+  },
+});
